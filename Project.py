9bb445e80ac32a0372b6913f06b3a93664cdeb7d
d-een Bashir <mohameddinhbashir@gmail.com>
2023-01-11 16:11:50 +0300

    Update Project.py

--- Project.py
+++ Project.py
@@ -6,17 +6,12 @@ import random
 from secrets import choice
 import time
 
-
 print("Hello there,Welcome to my manga recommender :)")
-
 manga  =[ 'Akumetsu', 'Black clover', 'Dandadan', 'Dorehedoro', 'Fire Punch', 'Jagaaan', 'Asura', 'Kingdom', 'Mashle', 'Monster', 'Record of Ragnarok ', 'GOH', 'Vagabond', 'Tokyo Revengers']
 
-#time to let the algos decide
-#print(choice(manga))
-
-#lets kick it up  a notch shall we?
-#we will group them into manhua and manga , started and never touched based on my read history
-
+# lets kick it up  a notch shall we?
+# we will group them into manhua and manga , started and never touched based on my read history
+# call anilist api and sort based on genre
 Seinen = ['Kingdom', 'Monster', 'Record of Ragnarok', 'Tokyo Revengers', 'GOH', 'Dorehedoro', 'Asura']
 Shonen = ['Jagaaan', 'Mashle', 'Akumetsu', 'Predator', 'Boxer', 'Red Storm', 'Dandadan']
 Hentai = "I AM DISSAPOINTED IN YOU!"
@@ -24,8 +19,6 @@ Isekai = ['Trash Hero', 'Jobless Reincernation','Slime']
 genre = ['Shonen', 'Seinen', 'Hentai', 'Isekai']
 feel = print("Fancy a read? ")
 def reccomend():
-
-      #print('What do you feel like reading? ', genre) 
       print(genre)
       mood = input('What do you feel like reading? ')
 
@@ -39,17 +32,11 @@ def reccomend():
          print(Hentai)
 
       else: 
-         print(choice(Isekai))     
-
-   
+         print(choice(Isekai))       
       print("Have fun! :)")
-
 reccomend()
-
 Seinen.append("Death Note")
-
-Retry = input("Would you like to try again?, Press Y if yes: ")
-
+Retry = input("Didn't like the recommended manga?" /n "Would you like to try again?, Press Y if yes: ")
 if Retry == "Y":
       reccomend()
 else:
