bf12e06d586716a2e2c4a655c4a7375076aa8698
d-een <mohameddinhbashir@gmail.com>
2023-01-19 13:29:35 +0300

    Complete genre and studio recommender

--- Anime.py
+++ Anime.py
@@ -2,34 +2,35 @@ import json
 import random
 file= open("Anime.Json").read()
 data = json.loads(file)
-# use list index 
 
-for details in data:
-    stud = details.get('studio')
-    genre = details.get('genres')
-    title = details.get('title')    
-    desc = details.get('description')
-    rating = details.get('hype')
 def recommend():
     pref = input("Would you like to watch based on genre, studio or anime rating?\n(Select One): ")
     if pref == "studio":
+        print("----------You know your stuff----------")
         pick= input("Studio Name: \n")
         for choice in data:
             studio = choice.get('studio')
             genre = choice.get('genres')
             title = choice.get('title')
             desc = choice.get('description')
+            rating = choice.get('hype')
             if pick == studio:
-                print(studio,genre,title,desc)
-                
-    # categorize rating
+                print("Studio Name:",studio," Genres:",genre,"Name:",title,"Description",desc,"Rating",rating, "\n")
+            
     if pref == "genre":
-        print("-----------Still in Dev--------------")
-        gen = input("Genre: \n")
+        print("-----------Use One genre--------------")
+        pick = input("Genre: \n")
         for opt in data:
-            slct = opt.get('genres')
-            print(slct)
-            # match if gen is in range of element in list(slct)                
+            genre = opt.get('genres')
+            studio = opt.get('studio')
+            title = opt.get('title')
+            desc = opt.get('description')
+            rating = opt.get('hype')
+            genres = genre[::]
+            if pick in genres:
+                print("Studio Name:",studio," Genres:",genre,"Name:",title,"Description",desc,"Rating",rating, "\n")
+                        
+    # categorize rating
                 
 recommend()            
     
